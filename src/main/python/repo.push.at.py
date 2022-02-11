4f81ca9212464f9489a927218ef6b67ba826defb
Mārtiņš Avots <martins.avots@splitcells.net>
2022-02-11 19:51:40 +0100

    Implment `repo.push.at`.
    Signed-off-by: Mārtiņš Avots <martins.avots@splitcells.net>

---
+++ src/main/python/repo.push.at.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""
+This program and the accompanying materials are made available under the
+terms of the Eclipse Public License 2.0, which is available at
+http://www.eclipse.org/legal/epl-2.0, or the MIT License,
+which is available at https://spdx.org/licenses/MIT.html.
+"""
+
+__author__ = "Mārtiņš Avots"
+__authors__ = ["and other"]
+__copyright__ = "Copyright 2021"
+__license__ = "EPL-2.0 OR MIT"
+
+import argparse
+import logging
+import subprocess
+import sys
+
+from os import environ
+
+if __name__ == '__main__':
+	if environ.get('log_level') == 'debug':
+		logging.basicConfig(level = logging.DEBUG)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--remote-repo-name'
+				, dest = 'remoteRepoName'
+				, required = True
+				, help = 'This is the local name of the remote repository,to which data is pushed.')
+	parser.add_argument('--remote-repo-URL'
+		, dest = 'remoteRepoUrl'
+		, required = True
+		, help = 'This is the URL of the remote repository,to which data is pushed.')
+	parsedArgs = parser.parse_args()
+	
+	commandToExecute = "git remote add "\
+				+ parsedArgs.remoteRepoName\
+				+ " "\
+				+ parsedArgs.remoteRepoUrl\
+				+ " 2>/dev/null"
+	logging.debug("Executing: Ensure that remote label exists: " + commandToExecute)
+	subprocess.call(commandToExecute, shell='True')
+	
+	commandToExecute = "git remote set-url "\
+				+ parsedArgs.remoteRepoName\
+				+ " "\
+				+ parsedArgs.remoteRepoUrl\
+				+ " 2>/dev/null"
+	logging.debug("Executing: Set remote URL: " + commandToExecute)
+	subprocess.call(commandToExecute, shell='True')
+	
+	commandToExecute = "git remote set-url --push "\
+				+ parsedArgs.remoteRepoName\
+				+ " "\
+				+ parsedArgs.remoteRepoUrl\
+				+ " 2>/dev/null"
+	logging.debug("Executing: Set remote URL: " + commandToExecute)
+	subprocess.call(commandToExecute, shell='True')
+	
+	commandToExecute = "git push --all " + parsedArgs.remoteRepoName
+	logging.debug("Executing: Set remote URL: " + commandToExecute)
+	subprocess.call(commandToExecute, shell='True')
